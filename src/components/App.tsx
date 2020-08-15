c307ef904683b7cb9bdefee04f404c2cf193c23a
Alexey Oblomov <alexey.oblomov.1981@gmail.com>
2020-08-15 18:40:20 +0300

    add app

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import NewTask from "./NewTask";
+import TasksList from "./TasksList";
+
+const App = () => {
+
+    return (
+        <>
+            <NewTask />
+            <TasksList />
+        </>
+    )
+}
+
+export default App;
